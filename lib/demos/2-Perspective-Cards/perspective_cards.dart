0a4da611e7a5f1c2adc31a536b1a2930a0c091f8
wilsonowilson <wilsonwilsonme@gmail.com>
2020-12-10 18:18:08 +0100

    Perspective cards now use full parallax

--- lib/demos/2-Perspective-Cards/perspective_cards.dart
+++ lib/demos/2-Perspective-Cards/perspective_cards.dart
@@ -9,81 +9,68 @@ class PerspectiveCards extends StatefulWidget {
 
 class _PerspectiveCardsState extends State<PerspectiveCards>
     with SingleTickerProviderStateMixin {
-  AnimationController perspectiveController;
-  Animation<double> perspectiveAnimation;
-
-  @override
-  void initState() {
-    perspectiveController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 200));
-    perspectiveAnimation = Tween<double>(begin: -1, end: 1).animate(
-        CurvedAnimation(
-            curve: Curves.easeInOutCubic, parent: perspectiveController));
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    perspectiveController.dispose();
-    super.dispose();
-  }
+  double relativeDx = 0;
+  double relativeDy = 0;
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: MouseRegion(
-          onEnter: (e) {},
-          onHover: (e) {
-            final isLeft = e.localPosition.dx <= (screenWidth / 2);
-            if (!perspectiveController.isAnimating) {
-              if (isLeft)
-                perspectiveController.forward();
-              else
-                perspectiveController.reverse();
-            }
-          },
-          child: SizedBox.expand(
-            child: Center(
-              child: AnimatedBuilder(
-                  animation: perspectiveController,
-                  builder: (context, snapshot) {
-                    return Transform.translate(
-                      offset: Offset(0, -70),
-                      child: Stack(
-                        children: [
-                          Transform.translate(
-                            offset:
-                                Offset(100 * perspectiveAnimation.value, 160),
-                            child: Transform(
-                              alignment: FractionalOffset.center,
-                              transform: Matrix4.identity()
-                                ..setEntry(3, 2, 0.001)
-                                ..rotateY(0.3 * perspectiveAnimation.value),
-                              child: PerspectiveCard(
-                                imageAsset: 'assets/images/eiffel.jpeg',
-                              ),
-                            ),
-                          ),
-                          Transform.translate(
-                            offset: Offset(10 * perspectiveAnimation.value, 0),
-                            child: Transform(
-                              alignment: FractionalOffset.center,
-                              transform: Matrix4.identity()
-                                ..setEntry(3, 2, 0.001)
-                                ..rotateY(0.2 * perspectiveAnimation.value),
-                              child: PerspectiveCard(
-                                imageAsset: 'assets/images/colosseum.jpeg',
-                              ),
-                            ),
-                          ),
-                        ],
+      backgroundColor: Colors.black,
+      body: MouseRegion(
+        onHover: _manageOnHover,
+        child: SizedBox.expand(
+          child: Center(
+            child: Transform.translate(
+              offset: Offset(0, 0),
+              child: Stack(
+                children: [
+                  Transform.translate(
+                    offset: Offset(
+                      100 * -relativeDx,
+                      50 * -relativeDy,
+                    ),
+                    child: Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001)
+                        ..rotateY(0.3 * -relativeDx),
+                      child: PerspectiveCard(
+                        imageAsset: 'assets/images/eiffel.jpeg',
                       ),
-                    );
-                  }),
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset(10 * -relativeDx, 10 * -relativeDy),
+                    child: Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001)
+                        ..rotateY(0.2 * -relativeDx),
+                      child: PerspectiveCard(
+                        imageAsset: 'assets/images/colosseum.jpeg',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  void _manageOnHover(e) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final referenceWidth = (screenWidth) / 2;
+    final newRelativeDx =
+        (-((referenceWidth - e.localPosition.dx) / referenceWidth));
+    final referenceHeight = (screenHeight) / 2;
+    final newRelativeDy =
+        (-((referenceHeight - e.localPosition.dy) / referenceHeight));
+    setState(() => relativeDx = newRelativeDx);
+
+    setState(() => relativeDy = newRelativeDy);
   }
 }
